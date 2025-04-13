9191067a18227aa67ee79047d3c05ab51ddc9950
Sergiy Pavlyshyn <pavlyshynserjiy@gmail.com>
2025-04-13 15:33:43 +0300

    Додав таблиці до своїх функцій, трохи колорами, передбачення для пошуку імен, змінив трохи логіку функцій

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ COMMANDS = [
     "hello",
     'help',
     "add",
-    "addall",
     'add-note',
     "remove",
     "edit",
@@ -30,7 +29,6 @@ HELP = [
     'help - <>',
     "add",
     'add-note',
-    'addall',
     'remove',
     "edit",
     'delete',
@@ -54,64 +52,71 @@ def main():
     user_input = input("Enter a command: ")
     command, *args = parse_input(user_input)
     if command not in COMMANDS:
-            suggestion = suggest_command(command, COMMANDS)
-            if suggestion:
-                
+            suggestions = suggest_command(command, COMMANDS)
+            if suggestions:
+              for suggestion in suggestions:  
                 choice = input(f"Did you mean '{suggestion}'? (Y/N): ").strip().lower()
                 if choice == 'y':
                     command = suggestion
+                    break
+                elif choice == 'n':
+                  continue
                 else:
                     print("Invalid command. Please try again.")
-                    continue
-            else:
+                    break
+              else:
                 print("Invalid command. Please try again.")
-                continue
-            
+              continue
+            else:
+              print("Invalid command. Please try again.")
+              continue
 
-    if command in ["close", "exit"]:
-      save_data(book)
-      save_data(note_book, filename="notebook.pkl")
-      print("Good bye!\nSaving data...")
-      break
-    elif command == "hello":
-      print("How can I help you?")
-    elif command == "add":
-      print(add_contact(book))
-    elif command == "addall":
-      print(add_all(book))
-    elif command == "remove":
-      print(remove_contact_info(book))
-    elif command == "edit":
-      print(edit_contact_info(book))    
-    elif command == "delete":
-      print(remove_contact(book))
-    elif command == "phone":
-      print(show_phone(args, book))
-    elif command == "all":
-      print(show_all(book))
-    elif command == "show-birthday":
-      print(show_birthday(args, book))
-    elif command == "birthdays":
-      print(birthdays(args, book))         
-    elif command == 'search':
-      print(search(args, book))
-    elif command == "add-note":
-      print(add_note(args, note_book))
-    elif command == "show-all-notes":
-      print(show_notes(note_book))  
-    elif command == "delete-note":
-      print(delete_note(args, note_book))  
-    elif command == "add-tag":
-      print(add_tag(args, note_book))  
-    elif command == "find-tag":
-      print(find_by_tag(args, note_book))
-    elif command == 'help':
-      print("Available commands:")
-      for cmd in HELP:
-        print(f"- {cmd}")
-    else:
-      print("Invalid command.")
-        
+            
+    try:
+      if command in ["close", "exit"]:
+        save_data(book)
+        save_data(note_book, filename="notebook.pkl")
+        print("Good bye!\nSaving data...")
+        break
+      elif command == "hello":
+        print("How can I help you?")
+      elif command == "add":
+        print(add_contact(book))
+      elif command == "addall":
+        print(add_all(book))
+      elif command == "remove":
+        print(remove_contact_info(book))
+      elif command == "edit":
+        print(edit_contact_info(book))    
+      elif command == "delete":
+        print(remove_contact(book))
+      elif command == "phone":
+        print(show_phone(args, book))
+      elif command == "all":
+        print(show_all(book))
+      elif command == "show-birthday":
+        print(show_birthday(args, book))
+      elif command == "birthdays":
+        print(birthdays(args, book))         
+      elif command == 'search':
+        print(search(args, book))
+      elif command == "add-note":
+        print(add_note(args, note_book))
+      elif command == "show-all-notes":
+        print(show_notes(note_book))  
+      elif command == "delete-note":
+        print(delete_note(args, note_book))  
+      elif command == "add-tag":
+        print(add_tag(args, note_book))  
+      elif command == "find-tag":
+        print(find_by_tag(args, note_book))
+      elif command == 'help':
+        print("Available commands:")
+        for cmd in HELP:
+          print(f"- {cmd}")
+    except Exception as e:
+      print(f'{e}')
+          
 
 if __name__ == "__main__":
   main()
